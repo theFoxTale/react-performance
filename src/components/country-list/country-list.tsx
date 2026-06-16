4a5ead763c8adba6d804f8770e2f0366bf20dffe
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-06-16 03:56:39 +0300

    feat: update CountryCard index

--- src/components/country-list/country-list.tsx
+++ src/components/country-list/country-list.tsx
@@ -48,9 +48,9 @@ export const CountryList = ({
 
   return (
     <div className={styles.countryList}>
-      {filteredCountries.map((country, index) => (
+      {filteredCountries.map((country) => (
         <CountryCard
-          key={index}
+          key={country.id}
           country={country}
           selectedYear={selectedYear}
           selectedColumns={selectedColumns}
